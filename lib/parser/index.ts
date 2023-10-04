e4707d50a3341b06fc71c6afb87c074b7b8602e3
Adam Avenir <adam@andyet.com>
2023-10-04 15:39:02 -0700

    inital gh-pages commit

--- lib/parser/index.ts
+++
@@ -1,7 +0,0 @@
-// @ts-expect-error peggy gives us javascript, ignore it
-import * as parser from "./tree.js";
-import { type Node } from "../compiler";
-
-export function parse (threatdown: string): Node {
-  return parser.parse(threatdown) as Node;
-}
