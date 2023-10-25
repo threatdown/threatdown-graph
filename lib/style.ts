fc3341633adead83295dfefe01c220dc202ef2c3
Burgertime <114885827+burgertime-c4@users.noreply.github.com>
2023-10-25 15:45:36 -0400

    Adding colors and default styling for dark mode

--- lib/style.ts
+++ lib/style.ts
@@ -1,7 +1,36 @@
+const colors = {
+  white: "#ffffff",
+  n100: "#0c0b0e",
+  n90: "#1d1b26",
+  n85: "#22202d",
+  n80: "#2e2b3b",
+  n70: "#312e40",
+  n60: "#3e3b4e",
+  n50: "#4b4759",
+  n40: "#565266",
+  n30: "#6b6680",
+  n20: "#8d8a99",
+  n10: "#adabb2",
+  n5: "#c4c2cc",
+  n2: "#e6e6e6",
+  y5: "#ffe878",
+  y1: "#ffec91",
+  b80: "#4a3dff",
+  b70: "#5f3dff",
+  b60: "#7549ff",
+  b50: "#7f5bff",
+  b40: "#7f63ef",
+  b30: "#9385ff",
+  b20: "#9e99ff",
+};
+
+const defaults = {
+  strokeWidth: "2px",
+};
 // the contents of this variable get written to a temporary file and passed via --cssFile to the mermaid cli
 export const css = `
 @import url('https://fonts.googleapis.com/css2?family=DM+Sans:opsz@9..40&amp;display=swap');
-* {
+#my-svg * {
   font-family: 'DM Sans', sans-serif;
   word-wrap: break-word;
   white-space: normal;
@@ -11,17 +40,17 @@ export const css = `
 // these styles get inlined into the mermaid code
 export const styles = {
   dark: {
-    background: "black",
-    objective: "stroke:#880000",
-    condition: "stroke:#008800",
-    assumption: "stroke:#000088",
-    booleanAnd: "fill:#880000",
-    booleanOr: "fill:008800",
-    link: "stroke:#666666",
-    mitigatedLink: "stroke:#66AA66",
+    background: colors.n90,
+    objective: `fill:${colors.n100},color:${colors.white},stroke:${colors.n60},stroke-width:${defaults.strokeWidth}`,
+    condition: `fill:${colors.n100},color:${colors.white},stroke:${colors.n60},stroke-width:${defaults.strokeWidth}`,
+    assumption: `fill:${colors.n100},color:${colors.white},stroke:${colors.n60},stroke-width:${defaults.strokeWidth}`,
+    booleanAnd: `fill:${colors.b80},color:${colors.white},stroke:${colors.white},stroke-width:${defaults.strokeWidth}`,
+    booleanOr: `fill:${colors.b80},color:${colors.white},stroke:${colors.white},stroke-width:${defaults.strokeWidth}`,
+    link: `stroke:${colors.b80},stroke-width:${defaults.strokeWidth}`,
+    mitigatedLink: `stroke:${colors.b80},stroke-width:${defaults.strokeWidth}`,
     modifiers: {
-      mitigation: "",
-      complete: "color:#00FF00",
+      mitigation: `stroke:${colors.b80},stroke-width:${defaults.strokeWidth}`,
+      complete: `fill:${colors.n100},color:${colors.white},stroke:${colors.n60},stroke-width:${defaults.strokeWidth}`,
     },
   },
   light: {
@@ -31,7 +60,7 @@ export const styles = {
     assumption: "stroke:#0000FF",
     booleanAnd: "fill:#FF0000",
     booleanOr: "fill:#00FF00",
-    link: "stroke:#FFFFFF",
+    link: "stroke:${colors.white}",
     mitigatedLink: "stroke:#AAFFAA",
     modifiers: {
       mitigation: "",
